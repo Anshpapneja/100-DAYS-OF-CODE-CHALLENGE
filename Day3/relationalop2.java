0b960fd5bbc57d27335bd0ebc0773cacf8ff69e2
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-04-12 13:16:30 +0530

    Create relationalop2.java

---
+++ Day3/relationalop2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class relationalop2 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println((A!=B));
+    }    
+}
