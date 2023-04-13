bf396491769f446da1c0a8c1dd0eb0a5a064a74e
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-04-13 20:04:53 +0530

    Create logicalop.java

---
+++ Day4/logicalop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class logicalop {
+    public static void main(String[] args) {
+        System.out.println( (3>2) && (5>0) );
+    }
+}
+
+//OR operator:
+public static void main(String[] args) {
+    System.out.println( (3<2) || (5>2) );
+}
+
