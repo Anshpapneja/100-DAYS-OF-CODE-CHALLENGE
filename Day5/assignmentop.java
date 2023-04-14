01be4f1154831ab484650820a222a7d410857891
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-04-14 21:26:33 +0530

    Create assignmentop.java

---
+++ Day5/assignmentop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Assignmentop {
+    public static void main(String[] args) {
+        int A = 10;
+       // A = A + 10;
+       A += 10;
+       System.out.println(A);
+   }    
+}
