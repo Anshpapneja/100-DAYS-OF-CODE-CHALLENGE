768f9a0fa8ac397eb69cb79dc6472f9d9d423972
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-04-19 22:51:57 +0530

    Create ternaryop.java

---
+++ Day10/ternaryop.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ternaryop {
+    public static void main(String[] args) {
+        int number = 4; 
+        string type = ((number%2) == 0) ? "even" : "odd";
+        System.out.println(type);
+    }    
+}
