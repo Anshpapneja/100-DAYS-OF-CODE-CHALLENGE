0985e97c493fbfbd4e4184982eda93f3b186e28d
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-04-22 23:14:42 +0530

    Create calculator.java

---
+++ Day 12/calculator.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+import javax.swing.plaf.synth.SynthTreeUI;
+
+public class calculator {
+    public static void main(String[] args) {
+    Scanner Sc = new Scanner(System.in);
+    System.out.println("Enter a : ");
+    int a = Sc.nextInt();
+    System.out.println("Enter b : ");
+    int b = Sc.nextInt();
+    System.out.println("Enter operator : ");
+    char operator = Sc.next().charAt(0);
+
+    switch(operator){
+        case '+' : System.out.println(a+b);
+        break;
+        case '-' : System.out.println(a-b);
+        break;
+        case '*' : System.out.println(a*b);
+        break;
+        case '/' : System.out.println(a/b);
+        break;
+        case '%' : System.out.println(a%b);
+        break;
+        default : System.out.println("Wrong Operator");
+    }
+    
+  }
+
+}   
