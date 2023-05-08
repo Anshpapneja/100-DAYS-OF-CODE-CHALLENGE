558fc5c4423909afc6f104128917a71e11fbea01
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-05-08 14:54:40 +0530

    Create function2.java

---
+++ Day18/function2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class function2 {
+    public static void funtion2(String fname){
+        System.out.println(fname);
+    }
+public static void main(String[] args) {     
+     funtion2("Ansh Papneja");
+  }    
+}
