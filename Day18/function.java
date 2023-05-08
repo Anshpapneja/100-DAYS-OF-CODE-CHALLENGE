85c153904268ad40aa2a92dd8c1fa5286caabbb8
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-05-08 14:54:11 +0530

    Create function.java

---
+++ Day18/function.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class funtion {
+    public static void function(int a, int b){
+    System.out.println(a+b);
+    }
+public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    function(a,b);
+ }    
+}
