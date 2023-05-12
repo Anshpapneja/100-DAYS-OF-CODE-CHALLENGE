5cbe6cc3223df2baf4571655fc37fe878ebaa753
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-05-12 22:37:17 +0530

    Update oddeve.java

--- Day7/oddeve.java
+++ Day7/oddeve.java
@@ -4,6 +4,7 @@ public class oddeve {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
+         
 
         if (number % 2 == 0) {
             System.out.println("EVEN");
